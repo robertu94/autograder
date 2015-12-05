5287659bab0b34b3b159ef80becdb49174592e27
Robert Underwood <rr.underwood94@gmail.com>
2015-12-05 12:28:17 -0500

    Added tests that verify that build works as expected

---
+++ test/test_build.py
@@ -0,0 +1,51 @@
+"""
+This module is part of the Clemson ACM Auto Grader
+
+This module is responsible for testing the build functionality
+"""
+
+
+import subprocess
+import unittest
+from unittest import mock
+
+import build
+
+class BuildTests(unittest.TestCase):
+    """
+    A series of tests that validate the build functionality
+    """
+
+    def setUp(self):
+        self.student = {
+            'username': 'test',
+            'directory': 'test/scratch'
+            }
+
+        self.settings = {
+            'build': {
+                'timeout': 5,
+            },
+            'project': {'name': 'testproject'}
+        }
+
+    @mock.patch('build.subprocess.check_call')
+    def test_make(self, mock_check_call):
+        """
+        Test building a student project using make
+        """
+        build.build_make(self.settings, self.student)
+        mock_check_call.assert_called_with('make', stdout=subprocess.DEVNULL,
+                                           stderr=subprocess.DEVNULL, timeout=5,
+                                           cwd=self.student['directory'])
+
+    @mock.patch('build.subprocess.check_call')
+    def test_script(self, mock_check_call):
+        """
+        Test cleaning up with a script
+        """
+        self.settings['build']['command'] = 'date'
+        build.build_script(self.settings, self.student)
+        mock_check_call.assert_called_with('date', stdout=subprocess.DEVNULL,
+                                           stderr=subprocess.DEVNULL, shell=True, timeout=5,
+                                           cwd=self.student['directory'])

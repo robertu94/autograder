0f04ec5d350e996e561cf0046b053d792af8085d
Robert Underwood <rr.underwood94@gmail.com>
2015-12-18 21:56:01 -0500

    Add logging to update

--- autograder/update.py
+++ autograder/update.py
@@ -16,19 +16,21 @@ def update(settings, student):
     return:
         bool updated - whether or not changes were made to the repository
     """
-    UPDATERS = { "git": update_git,
-                "hg" : update_hg,
-                "noop": update_noop,
-                "svn": update_svn,
-                "script": update_script
-               }
-    return UPDATERS[settings.update.method](settings, student)
+    updaters = {
+        "git": update_git,
+        "hg" : update_hg,
+        "noop": update_noop,
+        "svn": update_svn,
+        "script": update_script
+    }
+    return updaters[settings.update.method](settings, student)
 
 
 def update_hg(settings, student):
     """
     Updates a Mercurial repository with the latest submission
     """
+    LOGGER.info('Beginning a HG update for student %s', student['username'])
     #hg incoming //check for updates
     #hg pull
     #hg update --clean
@@ -38,26 +40,26 @@ def update_git(settings, student):
     """
     Updates a Git repository with the latest submission
     """
-    #git clean -xdf
-    #git pull
-    #git submodule init
-    #git submodule update
+    LOGGER.info('Beginning a git update for student %s', student['username'])
     pass
 
 def update_noop(settings, student):
     """
     A No-Op updater
     """
+    LOGGER.info('Beginning a NOOP update for student %s', student['username'])
     return True
 
 def update_svn(settings, student):
     """
     Updates a SVN repository with the latest submission
     """
+    LOGGER.info('Beginning a svn update for student %s', student['username'])
 
 def update_script(settings, student):
     """
     Updates a SVN repository with the latest submission
     """
+    LOGGER.info('Beginning a script update for student %s', student['username'])
     #./scipt student
 

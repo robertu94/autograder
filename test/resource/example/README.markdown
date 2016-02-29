ee202e6e1ef6c032d16878f50655983ae08fcdc8
Robert Underwood <rr.underwood94@gmail.com>
2016-02-28 23:01:32 -0500

    Added example project

---
+++ test/resource/example/README.markdown
@@ -0,0 +1,9 @@
+This is a full example project with five students
+
+Here are the expected results:
+
+Student 1 - Perfect score
+Student 2 - Missing makefile
+Student 3 - Missing source file
+Student 4 - No output
+Student 5 - Wrong output
---
+++ test/resource/example/student5/fizzbuzz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	if (argc != 2) return 1;
+	if (sscanf(argv[1], "%d", &i) != 1) return 1;
+
+	if (i % 2 == 0){
+		printf("fizz");
+	}
+	if (i % 3 == 0){
+		printf("buzz");
+	}
+	printf("\n");
+	return 0;
+}
